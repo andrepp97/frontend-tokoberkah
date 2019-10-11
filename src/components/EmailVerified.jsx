c5a253f77eeb92cf8a6826d48d172e719680eb6d
andrepp97 <andreputerap@gmail.com>
2019-10-11 11:34:17 +0700

    resend email

--- src/components/EmailVerified.jsx
+++ src/components/EmailVerified.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import { urlApi } from '../helper/database'
 import queryString from 'query-string'
+import { confirmLogin } from "../redux/actions"
+import { connect } from 'react-redux';
 
 
 class EmailVerified extends Component {
@@ -16,10 +18,12 @@ class EmailVerified extends Component {
         axios.post(urlApi + 'user/emailConfirmed', {
             email: params.email
         }).then(res => {
+			console.log(res)
             this.setState({ message: 'Email Berhasil di Confirm' })
             localStorage.setItem('token', res.data.token)
             this.props.confirmLogin(res.data)
         }).catch(err => {
+			console.log(err.response)
             this.setState({ message: 'Email Gagal di Confirm' })
         })
     }
@@ -34,4 +38,4 @@ class EmailVerified extends Component {
     }
 }
 
-export default EmailVerified
+export default connect(null, { confirmLogin })(EmailVerified)
